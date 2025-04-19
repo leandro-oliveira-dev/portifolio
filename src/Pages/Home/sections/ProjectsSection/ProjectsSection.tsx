d46f6be61e041a3e508defcdf0b48fe196f106c8
LeandroOliveiraDev <lleandro.silva@outlook.com>
2025-04-19 17:36:42 -0300

    image restaurada

--- src/Pages/Home/sections/ProjectsSection/ProjectsSection.tsx
+++ src/Pages/Home/sections/ProjectsSection/ProjectsSection.tsx
@@ -1,6 +1,9 @@
 import { Box, Container, Grid, Typography, styled } from "@mui/material";
 import ProjectCard, { ProjectCardProps } from "../../../../components/ProjectCard/ProjectCard";
 import AnimationComponent from "../../../../components/AnimationComponent/AnimationComponent";
+import bibliotecaImg from '/src/assets/images/biblioteca_etec_image.png';
+import vetImg from '/src/assets/images/vet_pageInit.png';
+
 
 const ProjectsSection: React.FC = () => {
 
@@ -13,7 +16,7 @@ const ProjectsSection: React.FC = () => {
         {
             title: "Biblioteca Etec",
             subtitle: "Jan 2024 - Jun 2024",
-            srcImg: "/src/assets/images/biblioteca_etec_image.png",
+            srcImg: bibliotecaImg,
             description: "Sistema de administração da biblioteca da Etec da minha cidade, com funcionalidades como: cadastro de livros, armazenamento no acervo, cadastro de alunos, aluguel de livros, autenticação de alunos e administradores, histórico de movimentações, envio de e-mails para alunos e acesso a bibliotecas online.",
             technologies: "Technologies: Typescript, nodejs,Docker, Prisma, Postgress, React",
             websiteURL: "https://trello.com/",
@@ -22,7 +25,7 @@ const ProjectsSection: React.FC = () => {
         {
             title: "Clinica Veterinária",
             subtitle: "Jan 2025 - Em andamento",
-            srcImg: "/src/assets/images/vet_pageInit.png",
+            srcImg: vetImg,
             description: "A ideia deste sistema é cadastrar clientes e pets para realizar o controle de agendamentos com os veterinários disponíveis no momento, com envio automático de e-mails para os clientes. Futuramente, haverá também uma loja para pets, onde o cliente poderá adicionar itens ao carrinho e finalizar a compra.",
             technologies: "Technologies: Typescript, nodejs,Docker, Prisma, Postgress, React",
             websiteURL: "https://trello.com/",
